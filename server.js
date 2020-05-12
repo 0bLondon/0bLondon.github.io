24dae142232d89379d95859a83530e8b0a0a8271
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2020-05-11 20:09:28 -0400

    Cross origin fix

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, '.')));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.post('/sudoku_solver', (req, res) => {
 
     const { spawn } = require('child_process');
